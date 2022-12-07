0ca071ff7d86c7c3353cefe6ce0db811c83de262
adam urbanowicz <adam.urbanow@gmail.com>
2022-12-07 09:21:51 +0100

    fix

---
+++ westeros/westeros-movies-client/src/main/java/com/westeros/moviesclient/MoviesClient.java
@@ -0,0 +1,48 @@
+package com.westeros.moviesclient;
+
+import com.westeros.moviesclient.contract.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+
+public class MoviesClient implements IMoviesClient {
+
+    RestTemplate restClient;
+    String baseUrl;
+    String apiKey;
+    int version;
+
+    public MoviesClient(IMoviesClientSettings settings) {
+        restClient = new RestTemplate();
+        this.baseUrl=settings.getBaseUrl();
+        this.apiKey= settings.getApiKey();
+        this.version= settings.getApiVersion();
+    }
+
+    @Override
+    public PagedResultDto getByDateRange(LocalDate from, LocalDate to) {
+        return null;
+    }
+
+    @Override
+    public PagedResultDto getByDateRange(LocalDate from, LocalDate to, int page) {
+        return null;
+    }
+
+    @Override
+    public MovieDto getMovie(int id) {
+        String url = baseUrl+"/"+version+"/movie/"+id;//brakuje api_key
+        var response = restClient.getForEntity(url, MovieDto.class).getBody();
+        return response;
+    }
+
+    @Override
+    public CreditsDto getCredits(int id) {
+        return null;
+    }
+
+    @Override
+    public ActorDto getActorDetails(int id) {
+        return null;
+    }
+}
